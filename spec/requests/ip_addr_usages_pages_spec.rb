a34e27728414fb80dde8fc1aa6d22cbc78aeb07e
y_sasaki <vagrant@precise32.(none)>
2013-08-27 15:05:39 +0900

    edit page test

--- spec/requests/ip_addr_usages_pages_spec.rb
+++ spec/requests/ip_addr_usages_pages_spec.rb
@@ -24,5 +24,32 @@ describe "IP address usage pages" do
       it { should have_selector('div.alert.alert-success') }
       it { should have_content('割り当て済みIPアドレス') }
     end
+
+    describe "with invalid information" do
+      before do
+        fill_in "IPアドレス", with: "192,168,15,10"
+        click_button "Save changes"
+      end
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name) { "y_sasaki" }
+      let(:new_machine_name) { "vagrant" }
+      let(:new_location) { "vm" }
+      before do
+        fill_in "利用者", with: new_name
+        fill_in "マシン名", with: new_machine_name
+        fill_in "位置", with: new_location
+        click_button "Save changes"
+      end
+
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_content('割り当て済みIPアドレス') }
+      it { should have_content(new_name) }
+      it { should have_content(new_machine_name) }
+      it { should have_content(new_location) }
+    end
   end
 end
