0e9e9f32d08049813a032d58ffa28beb354c7e2f
y_sasaki <vagrant@precise32.(none)>
2013-08-27 14:52:20 +0900

    edit page with no change test

--- app/controllers/ip_addr_usages_controller.rb
+++ app/controllers/ip_addr_usages_controller.rb
@@ -6,4 +6,20 @@ class IpAddrUsagesController < ApplicationController
   def edit
     @usage = IpAddrUsage.find(params[:id])
   end
+
+  def update
+    @usage = IpAddrUsage.find(params[:id])
+    if @usage.update_attributes(user_params)
+      flash[:success] = "Usage updated"
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def user_params
+    params.require(:ip_addr_usage).permit(:ip_address, :user,
+                                          :machine_name,
+                                          :location, :note)
+  end
 end
