7c76d27335da4bdc2d67e85e5fea7324799362bd
y_sasaki <vagrant@precise32.(none)>
2013-08-28 15:39:15 +0900

    AvailableIpAddress validation

--- app/models/available_ip_address.rb
+++ app/models/available_ip_address.rb
@@ -1,3 +1,6 @@
 class AvailableIpAddress < ActiveRecord::Base
   has_one :ip_addr_usage, dependent: :restrict_with_exception
+
+  validates :ip_address, ip_addr: true, presence: true,
+                         uniqueness: true
 end
--- spec/models/available_ip_address_spec.rb
+++ spec/models/available_ip_address_spec.rb
@@ -20,6 +20,40 @@ describe AvailableIpAddress do
     it "should throw exception when destroyed with ip_addr_usage " do
       lambda {raise @address.destroy}.should raise_error
     end
-
   end
+
+  describe "when ip_address is not present" do
+    before { @address.ip_address = "" }
+    it { should_not be_valid }
+  end
+
+  describe "when ip_address is invalid format" do
+    it "should be invalid" do
+      ip_addresses = %w[192.168.0,1 192.168.0.258]
+      ip_addresses.each do |invalid_ip_address|
+        @address.ip_address = invalid_ip_address
+        expect(@address).not_to be_valid
+      end
+    end
+  end
+
+  describe "when ip_address is valid format" do
+    it "should be valid" do
+      ip_addresses = %w[192.168.15.1 192.168.0.111 218.225.123.151]
+      ip_addresses.each do |invalid_ip_address|
+        @address.ip_address = invalid_ip_address
+        expect(@address).to be_valid
+      end
+    end
+
+    it "should be valid in IPv6" do
+      ip_addresses = %w[2001:0db8:bd05:01d2:288a:1fc0:0001:10ee]
+      ip_addresses.each do |invalid_ip_address|
+        @address.ip_address = invalid_ip_address
+        expect(@address).to be_valid
+      end
+    end
+  end
+
+
 end
