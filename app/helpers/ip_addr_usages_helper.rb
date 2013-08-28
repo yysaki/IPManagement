db3c5c4bfeb1ddc6a68261bf948fec2e89cce3a2
y_sasaki <vagrant@precise32.(none)>
2013-08-28 14:01:45 +0900

    sort index list by IPaddress

--- app/helpers/ip_addr_usages_helper.rb
+++ app/helpers/ip_addr_usages_helper.rb
@@ -1,2 +1,7 @@
 module IpAddrUsagesHelper
+  def sort_by_ip_address(ip_addr_usages)
+    ip_addr_usages.sort_by do |usage| 
+      usage.ip_address.split('.').map { |i| "%3d" % i.to_i } 
+    end 
+  end
 end
