2f1b434b1c1452c4137298d9d22d7a72adc022a7
y_sasaki <vagrant@yysaki.com>
2013-08-30 12:29:55 +0900

    added ping status color to index page

--- app/models/ip_addr_usage.rb
+++ app/models/ip_addr_usage.rb
@@ -14,6 +14,18 @@ class IpAddrUsage < ActiveRecord::Base
     available_ip_address <=> rhs.available_ip_address
   end
 
+  # failed_ping_historyの状態を表すbootstrapのCSSクラスを戻す
+  def status
+    case failed_ping_history
+    when 0
+      ""
+    when 1..(THRESHOLD-1)
+      "text-warning"
+    else
+      "text-error"
+    end
+  end
+
   # ping到達不能を示す閾値
   THRESHOLD = 7
   def IpAddrUsage.all_of_unreachable
