65368f9ac9f0f5dde4694ff7660b07dd95817a10
y_sasaki <vagrant@yysaki.com>
2013-08-30 18:55:31 +0900

    remove available_ip_addresses()

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,21 +9,11 @@ module ApplicationHelper
     end
   end
 
-  def available_ip_addesses(ip_addresses, now_ip_address = nil)
-    if now_ip_address
-      unused_ip_addresses(ip_addresses).insert(0, now_ip_address)
-    else
-      unused_ip_addresses(ip_addresses)
-    end
-  end
-
   def unused_ip_addresses(ip_addresses)
     ans = AvailableIpAddress.all.sort.map { |add| add.ip_address }
-    
     ip_addresses.each do |ip_address|
       ans.delete(ip_address)
     end
-
     ans
   end
 end
